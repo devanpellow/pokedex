fe70056af7e550e68481849848a0dc75c14a40b9
Devan Pellow <pellod01@stepstone.com>
2021-02-21 20:34:41 -0500

    Fix missing photo

--- src/Home.js
+++ src/Home.js
@@ -11,20 +11,30 @@ const Home = () => {
     setUrl('https://pokeapi.co/api/v2/pokemon/' + param);
   };
 
+  const handleRandom = () => {
+    let param = (Math.floor(Math.random() * 932) + 1).toString();
+    setUrl('');
+    setUrl('https://pokeapi.co/api/v2/pokemon/' + param);
+  };
+
   return (
     <>
       <div className="logo-header bg-center h-28 bg-contain bg-no-repeat"></div>
       <form className="my-4" onSubmit={handleSubmit}>
         <input
-        placeholder="Name or #"
+          placeholder="Name or #"
           className="w-28 h-8 bg-blue-300 border-2 border-gray-700 rounded-md shadow-lg text-center"
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
         <button className="text-gray-900 " type="submit"></button>
-        {/* <input type="Random" value={Math.floor(Math.random() * 932)+1}/> */}
       </form>
+      <span
+        className="masterball cursor-pointer bg-center bg-contain bg-no-repeat outline-none"
+        type="Random"
+        onClick={() => handleRandom()}
+      ></span>
       <Pokedex url={url} />
     </>
   );
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -26,7 +26,7 @@ const Pokedex = ({ url }) => {
           </div>
           <div className="flex flex-col w-3/4 bg-gray-300 mx-auto border border-gray-400 rounded-2xl pt-4 px-4">
             <div className="h-40">
-              {pokemon && !shiny && (
+              {pokemon && pokemon.sprites.other.dream_world.front_default && !shiny && (
                 <img
                   className="border-2 border-black rounded-md bg-white"
                   src={pokemon.sprites.other.dream_world.front_default}
@@ -39,6 +39,17 @@ const Pokedex = ({ url }) => {
                   src={pokemon.sprites.front_shiny}
                   alt=""
                 />
+              )}
+              {pokemon && !pokemon.sprites.other.dream_world.front_default && (
+                <div className=" bg-white border-2 border-black rounded-md">
+                  <img
+                    className="border-2 border-black rounded-md bg-white"
+                    src={
+                      pokemon.sprites.other['official-artwork'].front_default
+                    }
+                    alt=""
+                  />
+                </div>
               )}
               {!pokemon && (
                 <div className="place-holder-ball bg-white bg-contain bg-no-repeat bg-center border-2 border-black rounded-md"></div>
@@ -94,7 +105,9 @@ const Pokedex = ({ url }) => {
               <div className="text-xl capitalize">Name: {pokemon.name}</div>
             )}
             {pokemon && (
-              <div className="text-lg">Height: {(pokemon.height / 10).toFixed(2)}m</div>
+              <div className="text-lg">
+                Height: {(pokemon.height / 10).toFixed(2)}m
+              </div>
             )}
             {isPending && <div className="text-lg">Calculating...</div>}
           </div>
@@ -169,13 +182,15 @@ const Pokedex = ({ url }) => {
           </div>
           <div className="flex flex-row">
             <div className="h-8 w-20 mx-4 bg-gradient-to-r from-yellow-500 to-yellow-400 rounded border border-red-600 shadow-lg flex justify-center items-center">
-            {pokemon && (
-              <div className="text-lg">{pokemon.types[0].type.name}</div>
-            )}
+              {pokemon && (
+                <div className="text-lg">{pokemon.types[0].type.name}</div>
+              )}
             </div>
             <div className="h-8 w-20 mx-4 bg-gradient-to-r from-yellow-500 to-yellow-400 rounded border border-red-600 shadow-lg flex justify-center items-center">
               {pokemon && (
-                <div className="text-xl">{pokemon.types[1] && (pokemon.types[1].type.name)}</div>
+                <div className="text-xl">
+                  {pokemon.types[1] && pokemon.types[1].type.name}
+                </div>
               )}
             </div>
           </div>
